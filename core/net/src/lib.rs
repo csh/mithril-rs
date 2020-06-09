69550934491668009d4cb998e15b60a15b3c5dde
Connor Spencer Harries <connor@harmony-labs.org.uk>
2020-06-09 17:06:43 +0100

    Implement support for JAGGRAB protocol using Tokio sockets.

--- core/net/src/lib.rs
+++ core/net/src/lib.rs
@@ -1,8 +1,12 @@
 #[macro_use] extern crate mithril_codegen;
 
+#[cfg(feature = "jaggrab")]
+pub use jaggrab::{JaggrabCodec, JaggrabFile, JaggrabError};
 pub use codec::RunescapeCodec;
 pub use packet::{cast_packet, Packet, PacketDirection, PacketId, PacketStage, PacketType, PacketLength};
 
+#[cfg(feature = "jaggrab")]
+mod jaggrab;
 mod codec;
 mod packet;
 
