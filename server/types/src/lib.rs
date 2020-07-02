ef7f417aa50dc406af4c702b842ee8e5fc82289d
Restyled.io <commits@restyled.io>
2020-07-02 22:24:03 +0000

    Restyled by rustfmt

--- server/types/src/lib.rs
+++ server/types/src/lib.rs
@@ -1,8 +1,8 @@
 pub mod auth;
 mod collision_detection;
-mod id_allocator;
 pub mod components;
+mod id_allocator;
 
 pub use collision_detection::CollisionDetector;
-pub use id_allocator::IdAllocator;
 pub use components::*;
+pub use id_allocator::IdAllocator;
